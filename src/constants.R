57f0d6b52cfa6030fdd3a6231ad043914c29299a
svdobbert <svenjadobbert@gmail.com>
2024-04-19 10:19:45 +0200

    feat: Add moving window correlation

--- src/constants.R
+++ src/constants.R
@@ -1,161 +1,261 @@
 # constants needed in all further analysis
 
 # Color palettes
-main_palette <- c("#00204d", "#7c7b78", "#ffea46", "darkred")
-palette_light <- rgb(col2rgb(main_palette)[1, ], col2rgb(main_palette)[2, ], col2rgb(main_palette)[3, ], max = 255, alpha = 150)
+main_palette <- c(
+  "#00204d",
+  "#7c7b78",
+  "#ffea46",
+  "darkred"
+)
 
-palette_elevation <- c("#00204d", "#7c7b78", "#ffea46")
+palette_light <- rgb(
+  col2rgb(main_palette)[1, ],
+  col2rgb(main_palette)[2, ],
+  col2rgb(main_palette)[3, ],
+  max = 255, alpha = 150
+)
 
-palette_meteo <- c("black", "darkred", "#00204d", "#52648d", "#7c7b78", "#cbba69", "#ffea46")
+palette_elevation <- c(
+  "#00204d",
+  "#7c7b78",
+  "#ffea46"
+)
 
-palette_ships <- c("#00204d", "#52648d")
+palette_meteo <- c(
+  "black",
+  "darkred",
+  "#00204d",
+  "#52648d",
+  "#7c7b78",
+  "#cbba69",
+  "#ffea46"
+)
+
+palette_ships <- c(
+  "#00204d",
+  "#52648d"
+)
+
+palette_moving_window <- c(
+  low = "#FFEA46",
+  mid = "#7C7B78",
+  high = "#00204D"
+)
+
+# define on-season (week)
+on_season_start <- 13
+on_season_end <- 43
 
 # meterological variables to be included
 meteo_var <- c(
-    "air_pressure",
-    "precipitation",
-    "relative_humidity",
-    "radiation00",
-    "radiation933",
-    "radiation1450",
-    "radiation_mean",
-    "temp00",
-    "temp770",
-    "temp933",
-    "temp1280",
-    "temp1450",
-    "temp_mean",
-    "inversion_ratio",
-    "UWD00",
-    "UWD14",
-    "VWD00",
-    "VWD14",
-    "mxWS00",
-    "mnWS00",
-    "avWS00",
-    "mxWS14",
-    "mnWS14",
-    "avWS14"
+  "air_pressure",
+  "precipitation",
+  "relative_humidity",
+  "radiation00",
+  "radiation933",
+  "radiation1450",
+  "radiation_mean",
+  "temp00",
+  "temp770",
+  "temp933",
+  "temp1280",
+  "temp1450",
+  "temp_mean",
+  "inversion_ratio",
+  "UWD00",
+  "UWD14",
+  "VWD00",
+  "VWD14",
+  "mxWS00",
+  "mnWS00",
+  "avWS00",
+  "mxWS14",
+  "mnWS14",
+  "avWS14"
 )
 
 meteo_elevation <- c(
-    "0 m a.s.l.",
-    "0 m a.s.l.",
-    "0 m a.s.l.",
-    "0 m a.s.l.",
-    "933 m a.s.l.",
-    "1450 m a.s.l.",
-    "average",
-    "0 m a.s.l.",
-    "770 m a.s.l.",
-    "933 m a.s.l.",
-    "1280 m a.s.l.",
-    "1450 m a.s.l.",
-    "average",
-    "none",
-    "0 m a.s.l.",
-    "1450 m a.s.l.",
-    "0 m a.s.l.",
-    "1450 m a.s.l.",
-    "0 m a.s.l.",
-    "0 m a.s.l.",
-    "0 m a.s.l.",
-    "1450 m a.s.l.",
-    "1450 m a.s.l.",
-    "1450 m a.s.l."
+  "0 m a.s.l.",
+  "0 m a.s.l.",
+  "0 m a.s.l.",
+  "0 m a.s.l.",
+  "933 m a.s.l.",
+  "1450 m a.s.l.",
+  "average",
+  "0 m a.s.l.",
+  "770 m a.s.l.",
+  "933 m a.s.l.",
+  "1280 m a.s.l.",
+  "1450 m a.s.l.",
+  "average",
+  "none",
+  "0 m a.s.l.",
+  "1450 m a.s.l.",
+  "0 m a.s.l.",
+  "1450 m a.s.l.",
+  "0 m a.s.l.",
+  "0 m a.s.l.",
+  "0 m a.s.l.",
+  "1450 m a.s.l.",
+  "1450 m a.s.l.",
+  "1450 m a.s.l."
 )
 
 temp_var <- c(
-    "T_00",
-    "T_07",
-    "T_09",
-    "T_12",
-    "T_14"
+  "T_00",
+  "T_07",
+  "T_09",
+  "T_12",
+  "T_14"
 )
 
 radiation_var <- c(
-    "R_00",
-    "R_09",
-    "R_14"
+  "R_00",
+  "R_09",
+  "R_14"
 )
 
 meteo_labels <- c(
-    "air pressure [hPa]",
-    "precipitation [mm]",
-    "relative humidity [%]",
-    "radiation [W/m²]",
-    "radiation [W/m²]",
-    "radiation [W/m²]",
-    "radiation [W/m²]",
-    "temperature [°C]",
-    "temperature [°C]",
-    "temperature [°C]",
-    "temperature [°C]",
-    "temperature [°C]",
-    "temperature [°C]",
-    "inversion ratio [°K]",
-    "wind direction (east/west)",
-    "wind direction (east/west)",
-    "wind direction (north/south)",
-    "wind direction (north/south)",
-    "average wind speed [m/s]",
-    "maximum wind speed [m/s]",
-    "minimum wind speed [m/s]",
-    "average wind speed [m/s]",
-    "maximum wind speed [m/s]",
-    "minimum wind speed [m/s]"
+  "air pressure [hPa]",
+  "precipitation [mm]",
+  "relative humidity [%]",
+  "radiation [W/m²]",
+  "radiation [W/m²]",
+  "radiation [W/m²]",
+  "radiation [W/m²]",
+  "temperature [°C]",
+  "temperature [°C]",
+  "temperature [°C]",
+  "temperature [°C]",
+  "temperature [°C]",
+  "temperature [°C]",
+  "inversion ratio [°K]",
+  "wind direction (east/west)",
+  "wind direction (east/west)",
+  "wind direction (north/south)",
+  "wind direction (north/south)",
+  "average wind speed [m/s]",
+  "maximum wind speed [m/s]",
+  "minimum wind speed [m/s]",
+  "average wind speed [m/s]",
+  "maximum wind speed [m/s]",
+  "minimum wind speed [m/s]"
 )
 
 # Levels
 site <- c("Geiranger", "Fjordcentre", "Dalen")
 elevation <- c("2 m a.s.l.", "90 m a.s.l.", "420 m a.s.l.")
 season <- c("spring", "summer", "autumn", "winter")
-months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+months <- c(
+  "Jan", "Feb",
+  "Mar", "Apr", "May",
+  "Jun", "Jul", "Aug", "Sep",
+  "Oct", "Nov", "Dec"
+)
 levels_elev <- c(
-    "none",
-    "average",
-    "0 m a.s.l.",
-    "770 m a.s.l.",
-    "933 m a.s.l.",
-    "1280 m a.s.l.",
-    "1450 m a.s.l."
+  "none",
+  "average",
+  "0 m a.s.l.",
+  "770 m a.s.l.",
+  "933 m a.s.l.",
+  "1280 m a.s.l.",
+  "1450 m a.s.l."
 )
 
 metadata <- c(
-    "site",
-    "elevation",
-    "date",
-    "year",
-    "month",
-    "hour",
-    "season"
+  "site",
+  "elevation",
+  "date",
+  "year",
+  "month",
+  "hour",
+  "season"
 )
 
 data_var <- c(
-    meteo_var,
-    "PM2.5"
+  meteo_var,
+  "PM2.5"
 )
 
 ships_labels <- c(
-    "Ferry",
-    "Hurtigruten dummy",
-    "All",
-    "Cruise ships"
+  "Ferry",
+  "Hurtigruten dummy",
+  "All",
+  "Cruise ships"
 )
 
 # Day and Night hours for Norway (Geiranger)
 # 0 = night, 1 = twilight, 2 = day
 day_night <- list(
-    jan = c(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0), # 10-16, twilight: 7-19
-    feb = c(0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0), # 8-17, twilight: 6-20
-    mar = c(0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0), # 7-18, twilight: 4-21
-    apr = c(0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1), # 6-21, twilight: 2-1
-    may = c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1), # 5-22, twilight: -
-    jun = c(1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1), # 4-23, twilight: -
-    jul = c(1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1), # 4-23, twilight: -
-    aug = c(1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1), # 6-21, twilight: -
-    sep = c(0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0), # 7-20, twilight: 4-23
-    oct = c(0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0), # 8-18, twilight: 6-21
-    nov = c(0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0), # 9-16, twilight: 6-18
-    dec = c(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0) # 10-15, twilight: 7-18
+  jan = c(
+    0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1,
+    0, 0, 0, 0, 0, 0
+  ),
+  # 10-16, twilight: 7-19
+  feb = c(
+    0, 0, 0, 0, 0, 0,
+    1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
+    0, 0, 0, 0, 0
+  ),
+  # 8-17, twilight: 6-20
+  mar = c(
+    0, 0, 0, 0,
+    1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
+    0, 0, 0, 0
+  ),
+  # 7-18, twilight: 4-21
+  apr = c(
+    0, 0,
+    1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1
+  ),
+  # 6-21, twilight: 2-1
+  may = c(
+    1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 1, 1
+  ),
+  # 5-22, twilight: -
+  jun = c(
+    1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 1
+  ),
+  # 4-23, twilight: -
+  jul = c(
+    1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 1
+  ),
+  # 4-23, twilight: -
+  aug = c(
+    1, 1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 1, 1, 1
+  ),
+  # 6-21, twilight: -
+  sep = c(
+    0, 0, 0, 0,
+    1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
+    0, 0
+  ),
+  # 7-20, twilight: 4-23
+  oct = c(
+    0, 0, 0, 0, 0, 0,
+    1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
+    0, 0, 0, 0
+  ),
+  # 8-18, twilight: 6-21
+  nov = c(
+    0, 0, 0, 0, 0, 0,
+    1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1,
+    0, 0, 0, 0, 0, 0, 0
+  ),
+  # 9-16, twilight: 6-18
+  dec = c(
+    0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 2, 2, 2, 2, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0
+  )
+  # 10-15, twilight: 7-18
 )
--- src/data-preperation.R
+++ src/data-preperation.R
@@ -10,7 +10,10 @@ unique(input$year)
 # include night and day distinction
 input_bymonth <- split(input, input$month)
 for (i in seq_along(input_bymonth)) {
-    input_bymonth[[i]]$day_night <- rep(day_night[[i]], nrow(input_bymonth[[i]]) / 24)
+  input_bymonth[[i]]$day_night <- rep(
+    day_night[[i]],
+    nrow(input_bymonth[[i]]) / 24
+  )
 }
 input <- as.data.frame(do.call(rbind, input_bymonth))
 day_night <- input$day_night
@@ -22,51 +25,65 @@ input$day_night <- day_night
 # rename months
 month <- input$month
 old <- unique(input$month)
-new <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun")
+new <- c(
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun"
+)
 month[month %in% old] <- new[match(month, old, nomatch = 0)]
 input$month <- month
 
-# calculate mean for temperature and radiation (measured at different elevations)
+# calculate mean for temperature and radiation
+# (measured at different elevations)
 input$T_mean <- rowMeans(input[, temp_var], na.rm = TRUE)
 input$R_mean <- rowMeans(input[, radiation_var], na.rm = TRUE)
 
 # restructure input
 input_restructured <- data.frame(
-    PM2.5 = c(input$G_PM2.5, input$C_PM2.5, input$D_PM2.5),
-    site = rep(site, each = nrow(input)),
-    elevation = rep(elevation, each = nrow(input)),
-    date = rep(input$Datetime_2, 3),
-    year = rep(input$year, 3),
-    month = rep(input$month, 3),
-    hour = rep(input$hour, 3),
-    season = get_season(input$Datetime_2),
-    day_night = rep(input$day_night, 3),
-    air_pressure = rep(input$AP_00, 3),
-    precipitation = rep(input$P_00, 3),
-    relative_humidity = rep(input$RH_00, 3),
-    radiation00 = rep(input$R_00, 3),
-    radiation933 = rep(input$R_09, 3),
-    radiation1450 = rep(input$R_14, 3),
-    radiation_mean = rep(input$R_mean, 3),
-    temp00 = rep(input$T_00, 3),
-    temp770 = rep(input$T_07, 3),
-    temp933 = rep(input$T_09, 3),
-    temp1280 = rep(input$T_12, 3),
-    temp1450 = rep(input$T_14, 3),
-    temp_mean = rep(input$T_mean, 3),
-    inversion_ratio = rep(input$IR_Bulk, 3),
-    UWD00 = rep(input$U_WD_00, 3),
-    UWD14 = rep(input$U_WD_14, 3),
-    VWD00 = rep(input$V_WD_00, 3),
-    VWD14 = rep(input$V_WD_14, 3),
-    mxWS00 = rep(input$mxWS_00, 3),
-    mnWS00 = rep(input$mnWS_00, 3),
-    avWS00 = rep(input$avWS_00, 3),
-    mxWS14 = rep(input$mxWS_14, 3),
-    mnWS14 = rep(input$mnWS_14, 3),
-    avWS14 = rep(input$avWS_14, 3),
-    ships = rep(input$Model_PM2.5_All_Ships_Contributors, 3),
-    model = rep(input$Model_PM2.5_All_Ships, 3)
+  PM2.5 = c(input$G_PM2.5, input$C_PM2.5, input$D_PM2.5),
+  site = rep(site, each = nrow(input)),
+  elevation = rep(elevation, each = nrow(input)),
+  date = rep(input$Datetime_2, 3),
+  year = rep(input$year, 3),
+  month = rep(input$month, 3),
+  hour = rep(input$hour, 3),
+  season = get_season(input$Datetime_2),
+  day_night = rep(input$day_night, 3),
+  air_pressure = rep(input$AP_00, 3),
+  precipitation = rep(input$P_00, 3),
+  relative_humidity = rep(input$RH_00, 3),
+  radiation00 = rep(input$R_00, 3),
+  radiation933 = rep(input$R_09, 3),
+  radiation1450 = rep(input$R_14, 3),
+  radiation_mean = rep(input$R_mean, 3),
+  temp00 = rep(input$T_00, 3),
+  temp770 = rep(input$T_07, 3),
+  temp933 = rep(input$T_09, 3),
+  temp1280 = rep(input$T_12, 3),
+  temp1450 = rep(input$T_14, 3),
+  temp_mean = rep(input$T_mean, 3),
+  inversion_ratio = rep(input$IR_Bulk, 3),
+  UWD00 = rep(input$U_WD_00, 3),
+  UWD14 = rep(input$U_WD_14, 3),
+  VWD00 = rep(input$V_WD_00, 3),
+  VWD14 = rep(input$V_WD_14, 3),
+  mxWS00 = rep(input$mxWS_00, 3),
+  mnWS00 = rep(input$mnWS_00, 3),
+  avWS00 = rep(input$avWS_00, 3),
+  mxWS14 = rep(input$mxWS_14, 3),
+  mnWS14 = rep(input$mnWS_14, 3),
+  avWS14 = rep(input$avWS_14, 3),
+  ships = rep(input$Model_PM2.5_All_Ships_Contributors, 3),
+  model = rep(input$Model_PM2.5_All_Ships, 3)
 )
 
 # get day from date column
